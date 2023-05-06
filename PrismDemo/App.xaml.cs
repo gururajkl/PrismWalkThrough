5f6124504d5e52bc5bb3917a8f80cd92fb751ecf
Gururaj KL <gururajragavendra7@gmail.com>
2023-05-06 17:12:30 +0530

    Through the walk through

---
+++ PrismDemo/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Prism.DryIoc;
+using Prism.Ioc;
+using PrismDemo.Views;
+using System.Windows;
+
+namespace PrismDemo
+{
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<ShellWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
