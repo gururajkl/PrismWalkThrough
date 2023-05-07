c86e2dc32ee35fb6922e173692d5f30b2f3b76e8
Gururaj KL <gururajragavendra7@gmail.com>
2023-05-07 14:41:06 +0530

    Working with prism modules

--- PrismDemo/App.xaml.cs
+++ PrismDemo/App.xaml.cs
@@ -1,5 +1,7 @@
-﻿using Prism.DryIoc;
+﻿using ModuleA;
+using Prism.DryIoc;
 using Prism.Ioc;
+using Prism.Modularity;
 using Prism.Regions;
 using PrismDemo.Core.Regions;
 using PrismDemo.Views;
@@ -25,6 +27,12 @@ namespace PrismDemo
             base.ConfigureRegionAdapterMappings(regionAdapterMappings);
             regionAdapterMappings.RegisterMapping(typeof(StackPanel),
                 Container.Resolve<StackPanelRegionAdapter>());
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            base.ConfigureModuleCatalog(moduleCatalog);
+            moduleCatalog.AddModule<ModuleAModule>();
         }
     }
 }
